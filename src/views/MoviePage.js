36f82c1f04715c3dcccb6e9bb92e57c5b2c58231
shashank-100 <shashank.telkhade@gmail.com>
2020-12-01 22:57:19 +0530

    Update MoviePage.js

--- src/views/MoviePage.js
+++ src/views/MoviePage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "../styles/styles.scss";
 
-import MovieCards from "../components/MovieCards";
+import MovieCards from "../components/movieCards";
 import MovieDetails from "../components/MovieDetails";
 import SearchComponent from "../components/SearchComponent";
 
